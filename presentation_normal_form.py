c70363c7b70b34cb5fa0d0bdbbc0cceb9603174f
root <root@RStambaugh>
2024-08-06 18:18:22 +0100

    New directory for isomorphism bit text files, new comments in presentation_normal_form

--- presentation_normal_form.py
+++ presentation_normal_form.py
@@ -2,7 +2,8 @@ from libsemigroups_pybind11 import Presentation, presentation
 
 #Given a presentation p, computes the canonical form of the presentation
 #by removing redundant generators and rules. Also validates the presentation
-#and sorts the rules. 
+#and sorts the rules. The code additionally normalizes the presentation's 
+#alphabet, then reduces the complement rules, then restores the old alphabet.
 def pres_gen_min(p):
     
     pp = Presentation(p.alphabet())
